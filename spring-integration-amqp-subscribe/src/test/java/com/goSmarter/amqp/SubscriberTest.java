1fd7973ba0911c58505cef6b6e87f9018098ead6
myfeike <liuyuhua69@gmail.com>
2013-10-27 19:04:00 +0800

    组织导入

--- spring-integration-amqp-subscribe/src/test/java/com/goSmarter/amqp/SubscriberTest.java
+++ spring-integration-amqp-subscribe/src/test/java/com/goSmarter/amqp/SubscriberTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring-integration-context.xml"})
