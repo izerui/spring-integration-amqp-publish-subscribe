3bea1644d8542cff3ca13b1b29bc53aed6020e5d
myfeike <liuyuhua69@gmail.com>
2013-10-27 18:05:15 +0800

    调试信息,debug test

--- spring-integration-amqp-publish/src/test/java/com/goSmarter/amqp/PublisherSubscriberTest.java
+++ spring-integration-amqp-publish/src/test/java/com/goSmarter/amqp/PublisherSubscriberTest.java
@@ -10,18 +10,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.integration.Message;
-import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.message.GenericMessage;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:spring-integration-context.xml"})
+@ContextConfiguration({"classpath:spring-integration-context.xml","classpath:spring-integration-context-stream.xml"})
 public class PublisherSubscriberTest {
 
 	@Autowired
 	@Qualifier("fanoutChannel")
-	PublishSubscribeChannel messageChannel;
+	MessageChannel messageChannel;
 
 	@Test
 	public void testIntegration() {
@@ -31,7 +32,9 @@ public class PublisherSubscriberTest {
 
 			Message<String> message = MessageBuilder.withPayload(request)
 					.build();
-			
+			//或者通过GenericMessage 生成msg
+//			GenericMessage<String> msg = new GenericMessage<String>(request);
+//			messageChannel.send(msg);
 			messageChannel.send(message);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
